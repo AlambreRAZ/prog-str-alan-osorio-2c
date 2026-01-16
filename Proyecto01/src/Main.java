4eb75cab189081dcbab7efea52e54245b8bda1c7
alexyafk <20253ds109@utez.edu.mx>
2026-01-16 14:09:48 -0600

    base conficto

--- Proyecto01/src/Main.java
+++ Proyecto01/src/Main.java
@@ -1,3 +1,9 @@
-public static void main(String[] args) {
-    System.out.println();
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Semana 2");
+        System.out.println("Version Base");
+        System.out.println("2+2 =4");
+        System.out.println("5*3 = 15");
+    }
 }
