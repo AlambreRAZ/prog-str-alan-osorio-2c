4479b71251a6ddd0023a39d1708d3de328e759af
AlambreRAZ <20253ds065@utez.edu.mx>
2026-01-26 21:24:13 -0600

    Agrega proyecto ActividadesBuenasPracticas

---
+++ ActividadBuenasPracticas/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = pedirEntero(scanner);
+        int resultado = sumarHastaN(numero);
+
+        System.out.println("La suma de los números del 1 hasta " + numero + " es: " + resultado);
+
+        scanner.close();
+    }
+
+    public static int pedirEntero(Scanner scanner) {
+        System.out.print("Ingrese un número entero: ");
+        return scanner.nextInt();
+    }
+
+    public static int sumarHastaN(int numero) {
+        int suma = 0;
+
+        for (int i = 1; i <= numero; i++) {
+            suma += i;
+        }
+
+        return suma;
+    }
+}
